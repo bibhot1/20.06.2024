073ed2895c6d530b01824765f04a6b41e2351e93
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-21 06:52:41 +1200

    Add project files.

---
+++ 20.06.2024/Program.cs
@@ -0,0 +1,11 @@
+﻿// methods
+
+// lets have a method
+
+void myFirstMethod()
+{
+    Console.WriteLine("Hello");
+    global::System.Console.WriteLine(" Your first method have been called");
+}
+myFirstMethod();
+myFirstMethod();
